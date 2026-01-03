aabb649ae60f4564acc94d52daeb5874e50bf256
Siddhartha <92244610+Witty-31-06@users.noreply.github.com>
2026-01-03 16:24:29 +0530

    feat: add support for unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 use core::panic::PanicInfo;
 
@@ -9,6 +13,8 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
@@ -17,4 +23,20 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 2);
+    println!("[ok]");
 }
