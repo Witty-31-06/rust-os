73483b95858029ad929cf7ce6e74f7bc3eb4f497
Siddhartha <92244610+Witty-31-06@users.noreply.github.com>
2026-01-05 22:51:52 +0530

    chore: move logic to serial.rs for integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-
+use rust_os::println;
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -16,6 +13,7 @@ pub extern "C" fn _start() -> ! {
 
     #[cfg(test)]
     test_main();
+
     loop {}
 }
 
@@ -30,41 +28,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
+    rust_os::test_panic_handler(info)
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 0);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
